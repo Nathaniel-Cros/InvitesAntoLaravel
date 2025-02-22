51b6bb224725b5f76ee36000f56b0d064684389b
Nathaniel Ruiz <oscnathanielrp@gmail.com>
2025-02-22 17:05:37 -0600

    feat: change video

--- resources/js/components/firstSection/index.jsx
+++ resources/js/components/firstSection/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const FirstSectionComponent = () => {
     return (
         <section className='flex flex-col w-screen h-screen items-center justify-center bg-[#010310]'>
-            <video src="http://files.nathaniel-ruiz.com/bg-esc.webm" className='w-full h-full z-0' autoPlay loop playsInline/>
+            <video src="http://files.nathaniel-ruiz.com/bg-esc.mp4" className='w-full h-full z-0' autoPlay loop playsInline/>
         </section>
     )
 }
